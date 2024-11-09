e8b463e039c6531900d97dfe697836957127d25e
MichaelLudwig01 <ludwig-michael1@gmx.net>
2024-11-09 21:38:53 +0100

    hurensohn

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
 from gpiozero import *
 import time
+import requests
+import json
 
 bridge = "192.168.178.21"
-auth = "gi3EDZaV8eCaB159ZWsDeHaxPRhB4Dr8CCGpmR6O"
+auth = "HJmaGj86m7iUN0TSgKyzUo9TU4Eg8nFctr9bwiFI"
+light_4 = 4
+light_5 = 5
+url_1 = f"https://{bridge}/api/{auth}/lights/{light_4}/state"
+url_2 = f"https://{bridge}/api/{auth}/lights/{light_5}/state"
+print(url_1)
+print(url_2)
 
-response = requests.get(f"https://{bridge}/api/{auth}/lights", verify=False)
+on = json.dumps({
+"on": True, "sat":254, "bri":254,"hue":283 
+})
 
-light1on = {
-    'which':4,
-    'data':{
-        'state':{'on':True}
-    }
-}
-
-light2on = {
-    'which':5,
-    'data':{
-        'state':{'on':True}
-    }
-}
-
-light1off= {
-    'which':4,
-    'data':{
-        'state':{'on':False}
-    }
-}
-
-light2off= {
-    'which':5,
-    'data':{
-        'state':{'on':False}
-    }
-}
-
-bridge.light.update(resource)
+off = json.dumps({
+"on": False
+})
 
 led = LED(17)
 pir = MotionSensor(24)
@@ -42,11 +26,21 @@ pir = MotionSensor(24)
 while True:
     pir.wait_for_motion()
     led.on()
-    bridge.light.update(light1on)
-    bridge.light.update(light2on)
-    print("You moved")
-    pir.wait_for_no_motion()
+    requests.request("PUT", url_1, data=on, verify=False)
+    print("an")
+    time.sleep(0.5)
+    print("aus")
     led.off()
-    bridge.light.update(light1off)
-    bridge.light.update(light2off)
-    print("Dario ist ein Hurensogn")
+    requests.request("PUT", url_1, data=off, verify=False)
+    time.sleep(0.5)
+    led.on()
+    requests.request("PUT", url_1, data=on, verify=False)
+    requests.request("PUT", url_2, data=on, verify=False)
+    print("Bewegung erkannt")
+    
+    pir.wait_for_no_motion()
+    time.sleep(10)
+    led.off() #hallo bekka!was geht ab? hiiier!!! HAllloo
+    requests.request("PUT", url_1, data=off, verify=False)
+    requests.request("PUT", url_2, data=off, verify=False)
+    print("Keine Bewegung mehr")
